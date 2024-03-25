1396eb0b66d09925b2c199645fa530c68ce38a9f
gipulina <georgi_palavecino@hotmail.com>
2024-03-25 20:02:16 -0300

    Paciente

---
+++ internal/domain/paciente.go
@@ -0,0 +1,10 @@
+package domain
+
+type Odontologo struct {
+	Id          int    `json:"id"`
+	Nombre      string `json:"nombre" binding:"required"`
+	Apellido    string `json:"apellido" binding:"required"`
+	Domicilio   string `json:"domicilio" binding:"required"`
+	DNI         int    `json:"dni" binding:"required"`
+	AltaSistema string `json:"altasistema" binding:"required"`
+}
